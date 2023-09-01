45e7578f812b477764965d5b4db0223c95be2245
Mason Harniess <maseharniess@outlook.com>
2023-09-01 13:19:00 +0100

    add shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,16 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
+  // define the components, directives, and pipes (CDP) that belong to this module
   declarations: [
     AppComponent,
     HomeComponent
   ],
+  // extend this module by importing capabilities of other modules
   imports: [
     BrowserModule
   ],
+  // register services (old practice — use providedin property of the service itself)
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+declarations: [],
+
+imports: [CommonModule],
+// share things from this shared module with other components
+exports: []
+})
+export class SharedModule { }
